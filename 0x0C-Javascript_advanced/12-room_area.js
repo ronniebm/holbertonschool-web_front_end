7d4db41903761ab14ed42d928dd62096f9958120
Ronnie <ronnie.coding@gmail.com>
2021-06-27 15:25:44 -0500

    adding task12

---
+++ 0x0C-Javascript_advanced/12-room_area.js
@@ -0,0 +1,12 @@
+let roomDimensions = {
+    width: 50,
+    length: 100,
+    getArea: function () {
+        return this.width * this.length
+    }
+}
+
+let boundGetArea = roomDimensions.getArea.bind(roomDimensions);
+
+// Test code
+boundGetArea();
