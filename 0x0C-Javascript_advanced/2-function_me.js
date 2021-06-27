d57c8ef0469b12b2410650126b3b426982cb74f4
Ronnie <ronnie.coding@gmail.com>
2021-06-27 15:18:21 -0500

    adding task2

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,14 @@
+function welcomeMessage (fullName) {
+    return function () {
+        alert('Welcome '+ fullName)
+    }
+}
+
+let guillaume = welcomeMessage('Guillaume');
+let alex = welcomeMessage('Alex');
+let fred = welcomeMessage('Fred');
+
+//How to test:
+guillaume();
+alex();
+fred();
